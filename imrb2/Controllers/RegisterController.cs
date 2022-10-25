a45d3e7649c0f33761429d75367b4219dc62876b
AlecMoore <alec.moore47@yahoo.com>
2022-10-25 07:44:27 +0100

    Initial commit

---
+++ imrb2/Controllers/RegisterController.cs
@@ -0,0 +1,36 @@
+﻿using imrb2.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace imrb2.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly ILogger<RegisterController> _logger;
+
+        public RegisterController(ILogger<RegisterController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Register(RegisterModel model)
+        {
+            RegisterModel test = model;
+            return RedirectToAction("/Home/Privacy");
+        }
+    }
+}
---
+++ imrb2/Models/RegisterModel.cs
@@ -0,0 +1,10 @@
+namespace imrb2.Models
+{
+    public class RegisterModel
+    {
+        string Username { get; set; }
+        string Email { get; set; }
+        string Password { get; set; }
+        string PasswordRepeat { get; set; }
+    }
+}
