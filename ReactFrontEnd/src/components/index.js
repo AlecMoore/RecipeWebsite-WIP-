fd0636db2eb1d11167621d518e5e36098ea9cf74
sambanks5 <121309218+sambanks5@users.noreply.github.com>
2023-01-08 23:01:16 +0000

    Sidebar creation

---
+++ ReactFrontEnd/src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Sidebar } from "./sidebar";
+export { default as SidebarItems } from "./sidebar/sidebarItems";
+export { default as DynamicItem } from "./routes/[item]";
+
+export { SIDEBAR_DATA as dummyData } from "./data";
