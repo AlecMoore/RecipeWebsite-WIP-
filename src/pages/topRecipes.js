606c77629bdd7e64a338f46fd0fcde172be23ef2
sambanks5 <121309218+sambanks5@users.noreply.github.com>
2023-01-10 00:25:56 +0000

    Navbar

---
+++ src/pages/topRecipes.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class topRecipes extends Component {
+  render() {
+    return (
+      <div>topRecipes</div>
+    )
+  }
+}
+
+export default topRecipes
